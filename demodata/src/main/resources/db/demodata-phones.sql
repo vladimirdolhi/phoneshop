a07765fde98427a3ddd916639fc1aad4af366030
Andrei Asipenka <aosipenko@tacitknowledge.com>
2017-09-21 18:18:05 +0300

    Added demodata module.

---
+++ demodata/src/main/resources/db/demodata-phones.sql
@@ -0,0 +1,7 @@
+insert into phones(id, model, color, displaySize, width, length, camera, price) VALUES
+  ('1', 'fly', 'white', 11, NULL ,NULL ,NULL, 11111),
+  ('2', 'samsung', 'white', 22,NULL ,NULL , NULL, 22222),
+  ('3', 'hts', 'white', 33, NULL ,NULL , NULL, 33333),
+  ('4', 'globo', 'white', 44, NULL ,NULL , NULL, 44444),
+  ('5', 'iphone', 'white', 55, NULL ,NULL , NULL, 99999);
+;
