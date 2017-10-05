e9f14b2943cfaf23afd7b9a472dd4901ecc6eb2a
Andrei Asipenka <aosipenko@tacitknowledge.com>
2017-10-05 13:59:28 +0300

    Added order page admin controller stub.

---
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/admin/OrdersPageController.java
@@ -0,0 +1,9 @@
+package com.es.phoneshop.web.controller.pages.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/admin/orders")
+public class OrdersPageController {
+}
